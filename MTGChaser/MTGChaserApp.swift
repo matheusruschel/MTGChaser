c847438570c7a19c046eea9df4af4b7491238823
Bruna <matheus.ruschel@gmail.com>
2025-04-07 12:57:37 -0400

    First commit

---
+++ MTGChaser/MTGChaserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTGChaserApp.swift
+//  MTGChaser
+//
+//  Created by Bruna on 2025-04-07.
+//
+
+import SwiftUI
+
+@main
+struct MTGChaserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MTGChaserTests/MTGChaserTests.swift
@@ -0,0 +1,16 @@
+//
+//  MTGChaserTests.swift
+//  MTGChaserTests
+//
+//  Created by Bruna on 2025-04-07.
+//
+
+import Testing
+
+struct MTGChaserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
